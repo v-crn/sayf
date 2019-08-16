72b1fc3dfe0b41a87096c56ea38ee70278016622
v-crn <var.cyrano@gmail.com>
2019-08-17 08:31:54 +0900

    modify location of config lines in carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,18 +12,18 @@ if Rails.env.production?
       :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
       :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
-    config.fog_directory     =  ENV['S3_BUCKET']
+		config.fog_directory     =  ENV['S3_BUCKET']
+		
+		# 公開・非公開の切り替え
+		config.fog_public     = true
+
+		# キャッシュの保存期間
+		config.fog_attributes = { 'Cache-Control' => "max-age=#{90.day.to_i}" }
+	
+		# キャッシュをS3に保存
+		config.cache_storage = :fog
+		
+		# ファイル名への日本語の使用を許可
+		CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
   end
-	
-	# 公開・非公開の切り替え
-  config.fog_public     = true
-
-  # キャッシュの保存期間
-  config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" }
-
-  # キャッシュをS3に保存
-  # config.cache_storage = :fog
-	
-	# ファイル名への日本語の使用を許可
-	# CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
 end
