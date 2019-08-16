89a6d42555bb81904acf19d6cee12c7a7dc7d60b
v-crn <var.cyrano@gmail.com>
2019-08-17 02:41:28 +0900

    add some configuration lines in carrierwave.rb to push heroku

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,11 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
 if Rails.env.production?
-  CarrierWave.configure do |config|
-    config.fog_provider = 'fog/aws'
+	CarrierWave.configure do |config|
+		config.storage = :fog
+		config.fog_provider = 'fog/aws'
 		config.fog_credentials = {
       :provider              => 'AWS',
       :region                => ENV['S3_REGION'],
