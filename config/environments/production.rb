c3c1aa9f820a39961320eba80e4eea98d90426c1
v-crn <var.cyrano@gmail.com>
2019-08-17 03:42:58 +0900

    activate raise_delivery_errors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "sayf_#{Rails.env}"
 
 	config.action_mailer.default_url_options = { :host => 'https://sayf-fictional-sayings.herokuapp.com/' }
-	config.action_mailer.raise_delivery_errors = false
+	config.action_mailer.raise_delivery_errors = true
 	config.action_mailer.perform_caching = false
 	config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
